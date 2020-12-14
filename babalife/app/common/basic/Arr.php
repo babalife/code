8a07f0b04c1dbeb8d94502dbc685cfda82c8c9c4
babalife <839475373@qq.com>
2020-12-14 08:59:50 +0800

    优化tree函数，获取权限选中数据

--- babalife/app/common/basic/Arr.php
+++ babalife/app/common/basic/Arr.php
@@ -9,9 +9,11 @@ class Arr
     /**
      * 分类树，支持无限极分类
      * @param $data
+     * @param string $parent 父节点名称
+     * @param string $child 子节点集合名称
      * @return array
      */
-    public static function getTree($data)
+    public static function getTree($data, $parent = 'pid', $child = 'list')
     {
         $items = [];
         // 数据格式化
@@ -22,9 +24,9 @@ class Arr
         $tree = [];
         foreach ($items as $id => $item) {
             // 通过pid找父类数组，找到则将本次数据添加至父类数组 list 中，跳出本次循环
-            if (isset($items[$item['pid']])) {
+            if (isset($items[$item[$parent]])) {
                 // 次级分类
-                $items[$item['pid']]['list'][] = &$items[$id];
+                $items[$item[$parent]][$child][] = &$items[$id];
             } else {
                 // 顶级分类
                 $tree[] = &$items[$id];
