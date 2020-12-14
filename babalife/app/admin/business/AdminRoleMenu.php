a11482e20c02a74ddd4370b916ea1b4bc1cc508f
babalife <839475373@qq.com>
2020-12-14 21:29:22 +0800

    新增角色菜单关联业务处理，校验类改名

---
+++ babalife/app/admin/business/AdminRoleMenu.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: babalife
+ * Date: 2020/12/14
+ * Time: 9:08 下午
+ * Git: https://github.com/babalife
+ */
+
+namespace app\admin\business;
+
+use app\admin\model\AdminRoleMenu as AdminRoleMenuModel;
+
+class AdminRoleMenu extends BaseBus
+{
+    public function __construct()
+    {
+        $this->model = new AdminRoleMenuModel();
+    }
+
+    // 设置角色菜单权限
+    public function setRoleMenu($data)
+    {
+        try {
+            if ($this->getByRoleId($data['role_id'])) {
+                // 存在角色则修改
+                $result = $this->model->where('role_id', $data['role_id'])->save($data);
+            } else {
+                // 不存在则新增
+                $result = $this->insertDate($data);
+            }
+        } catch (\Exception $e) {
+            $result = [];
+        }
+
+        return $result;
+    }
+
+    // 根据角色ID查询
+    public function getByRoleId($id)
+    {
+        try {
+            $result = $this->model->where('role_id', $id)->find();
+        } catch (\Exception $e) {
+            $result = [];
+        }
+
+        return $result;
+    }
+}
---
+++ babalife/app/admin/model/AdminRoleMenu.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: babalife
+ * Date: 2020/12/14
+ * Time: 9:06 下午
+ * Git: https://github.com/babalife
+ */
+
+namespace app\admin\model;
+
+
+class AdminRoleMenu extends BaseModel
+{
+
+}
